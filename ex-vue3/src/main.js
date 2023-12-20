a747d89d16e3f9927d9f0214e14e7505617c53cf
jmjmc <152248624+jmjmc@users.noreply.github.com>
2023-12-20 20:54:55 +0900

    2
    2

--- ex-vue3/src/main.js
+++ ex-vue3/src/main.js
@@ -1,6 +1,12 @@
 import './assets/main.css'
+import './assets/dist/css/bootstrap.min.css'
+import './assets/dist/js/bootstrap.bundle.min.js'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from '@/components/router'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router);
+app.use(App);
+app.mount('#app');
